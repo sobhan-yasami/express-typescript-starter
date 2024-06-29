880072ca036f2ed8f1e8e4e944a8ece580dbfa09
sobhan yasami <sobhan.yasami9170@gmail.com>
2024-06-29 16:50:32 +0330

    some minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
 import express, {Request, Response} from "express";
 
-
-
 const app = express();
-
-
 
 app.get('/', (req: Request, res: Response) => {
    res.send("hello from express-typescript app")
 })
-
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
